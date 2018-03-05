4ebd443a169e8f7780183941311cab0c7725d6b5
Fejer Norbert <norbyka97@gmail.com>
2018-03-05 13:30:44 +0200

    Added new items

---
+++ TyresStore/TyresStore.Repository/Interfaces/IVehiclesRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TyresStore.Repository.Models;
+
+namespace TyresStore.Repository.Interfaces
+{
+    interface IVehiclesRepository
+    {
+        Vehicle GetVehicleById(int vehicleId);
+
+        List<Vehicle> GetVehicle();
+    }
+}
---
+++ TyresStore/TyresStore.Repository/VehiclesRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TyresStore.Repository.Interfaces;
+using TyresStore.Repository.Models;
+
+namespace TyresStore.Repository
+{
+    public class VehiclesRepository : IVehiclesRepository
+    {
+        TyresStoreContext tyresContext = new TyresStoreContext();
+
+        public Vehicle GetVehicleById(int vehicleId)
+        {
+            return tyresContext.Vehicles.FirstOrDefault(x => x.ID == vehicleId);
+        }
+
+        public List<Vehicle> GetVehicle()
+        {
+            return tyresContext.Vehicles.ToList();
+        }
+    }
+}
